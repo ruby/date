ac50393758a7fe7d8a02a6ea2e46430e834a8302
Jean Boussier <jean.boussier@gmail.com>
2021-11-16 14:03:42 +0100

    check_limit: also handle symbols

--- ext/date/date_core.c
+++ ext/date/date_core.c
@@ -4337,6 +4337,7 @@ static void
 check_limit(VALUE str, VALUE opt)
 {
     if (NIL_P(str)) return;
+    if (SYMBOL_P(str)) str = rb_sym2str(str);
 
     StringValue(str);
     size_t slen = RSTRING_LEN(str);
